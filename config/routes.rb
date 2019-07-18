dd28627e9b441d603be79c078dc2bdbadad9bc5d
anhvan94 <nguyenanhvan1511@gmail.com>
2019-07-18 21:00:16 +0700

    18/7

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   	get 'static_pages/help'
   	get 'static_pages/about'
   	get  '/signup',  to: 'users#new'
+  	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
