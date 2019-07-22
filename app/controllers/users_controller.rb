2adad422978beca429b5cbf15c8619ea9548b647
anhvan94 <nguyenanhvan1511@gmail.com>
2019-07-22 17:25:18 +0700

    22/7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+  def create
+    @user = User.new(new_params)   
+    if @user.save
+
+    else
+      render 'new'
+    end
+  end
+  def new_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
